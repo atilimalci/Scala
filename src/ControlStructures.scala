f842d7ebd77c4d988bc9fd4ef2c3d3402b302183
atilimalci <admin@192.168.1.153>
2015-12-27 20:25:50 +0200

    First implementation

---
+++ src/ControlStructures.scala
@@ -0,0 +1,47 @@
+/**
+ * Examples of control structures.
+ */
+class ControlStructures {
+
+}
+
+object ControlStructures {
+
+  def main(args: Array[String]) {
+    //=====1
+    var i = 2;
+    var j = 3;
+
+    if (i > j) println(i) else println(j)
+    
+    
+    //=====2
+    val n = i > j match {
+      case true => i
+      case false => j
+    }
+    
+    println(s"Value of n is ${n}")
+    
+    //=====3
+    val day = "MON"
+    
+    val kind = day match {
+      case "MON" | "TUE" | "WED" | "THU" | "FRI" => "weekday"
+      case _ => "weekend"
+    }
+    
+    println(s"kind is: ${kind}")
+    
+    //=====4
+    val y : Any = 12180
+    
+    var t = y match {
+      case n : String => s"'n''"
+      case n : Double =>f"$n%.2f"
+      case n : Int => s"${n}i"
+    }
+    
+    println(s"value of t:${t}")
+  }
+}
